3a861f1eab81b0e1faf5d488db7273a3476813a8
Hendrik Röttgers <hrottgers@protonmail.com>
2026-08-24 22:10:59 +0200

    navBar

---
+++ src/ui/navBar.tsx
@@ -0,0 +1,11 @@
+export default function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">My Brew</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/status">Status</a></li>
+      </ul>
+    </nav>
+  );
+}
